b005acb85293d879e76fc21d3ab30c250bb4ed94
bharatmk257 <bharatmk257@gmail.com>
2019-01-09 21:41:31 +0530

    interface project created

---
+++ InterfaceJava/app/src/main/java/com/bharatmk257/interfacejava/MainActivity.java
@@ -0,0 +1,13 @@
+package com.bharatmk257.interfacejava;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
