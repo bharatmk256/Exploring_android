b2b29b7547a7bc93eb357023025cb18ddf95740d
bharatmk257 <bharatmk257@gmail.com>
2019-01-15 22:44:31 +0530

    animations and some logics with some bugs :D will solve tommorow

--- LionOrTiger/app/src/main/java/com/bharatmk257/lionortiger/MainActivity.java
+++ LionOrTiger/app/src/main/java/com/bharatmk257/lionortiger/MainActivity.java
@@ -15,13 +15,27 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    /**
+     * enum is an data type that we define also we have to define what input can it accept
+     */
+
     Player currentPlayer = Player.ONE;
+    /**
+     * we have define value of current player
+     */
 
 
     Player[] playerChoices = new Player[9];
+    /**
+     * create array fo player choices
+     */
 
     int[][] winnerRowsAndColumns = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8},
             {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {2, 4, 6}};
+
+    /**
+     * created 2 diamention array that checks who is winner
+     */
 
 
     @Override
@@ -32,14 +46,29 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void imageViewIsTapped(View imageView) {
+        /*
+         * created imageViewIsTapped method
+         * */
 
         ImageView tappedImageView = (ImageView) imageView;  // this is casting
+        /*
+         * imageView is tapped check
+         * */
 
         tappedImageView.setTranslationX(-2000);
+        /*
+         * make this image to other position so we can animate this image
+         * */
 
         int tiTag = Integer.parseInt(tappedImageView.getTag().toString());
+        /*
+         * get int of tapped image tag
+         * */
 
         playerChoices[tiTag] = currentPlayer;
+        /*
+         * check player choice and set it to array called playerChoices
+         * */
 
 
         if (currentPlayer == Player.ONE) {
@@ -49,12 +78,32 @@ public class MainActivity extends AppCompatActivity {
             tappedImageView.setImageResource(R.drawable.tiger);
             currentPlayer = Player.ONE;
         }
+        /*
+         * check which player is playing and change player every time
+         * */
 
 
         tappedImageView.animate().translationXBy(2000).alpha(1).
                 rotation(3600).setDuration(1000);
+        /*
+         * after tapping it animates every time
+         * */
 
         Toast.makeText(this, tappedImageView.getTag().toString(), Toast.LENGTH_SHORT).show();
+        /*toast tag of tapped image*/
+
+
+        for (int[] winnerColumns : winnerRowsAndColumns) {
+
+            if (playerChoices[winnerColumns[0]]
+                    == playerChoices[winnerColumns[1]]
+                    && playerChoices[winnerColumns[1]]
+                    == playerChoices[winnerColumns[2]]) {
+                Toast.makeText(this,"we have winner",Toast.LENGTH_SHORT).show();
+            }
+
+        }
+
 
     }
 }
