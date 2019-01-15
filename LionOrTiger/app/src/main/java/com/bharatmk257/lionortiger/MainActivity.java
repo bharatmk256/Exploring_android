1c1936d65bbf0ef7b6708853739ef9a5b74dcd86
bharatmk257 <bharatmk257@gmail.com>
2019-01-15 19:13:23 +0530

    created 2d array for identify user

--- LionOrTiger/app/src/main/java/com/bharatmk257/lionortiger/MainActivity.java
+++ LionOrTiger/app/src/main/java/com/bharatmk257/lionortiger/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +16,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     Player currentPlayer = Player.ONE;
+
+
+    Player[] playerChoices = new Player[9];
+
+    int[][] winnerRowsAndColumns = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8},
+            {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {2, 4, 6}};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,11 +37,15 @@ public class MainActivity extends AppCompatActivity {
 
         tappedImageView.setTranslationX(-2000);
 
+        int tiTag = Integer.parseInt(tappedImageView.getTag().toString());
+
+        playerChoices[tiTag] = currentPlayer;
+
 
         if (currentPlayer == Player.ONE) {
             tappedImageView.setImageResource(R.drawable.lion);
             currentPlayer = Player.TWO;
-        }else if (currentPlayer == Player.TWO){
+        } else if (currentPlayer == Player.TWO) {
             tappedImageView.setImageResource(R.drawable.tiger);
             currentPlayer = Player.ONE;
         }
@@ -41,6 +53,8 @@ public class MainActivity extends AppCompatActivity {
 
         tappedImageView.animate().translationXBy(2000).alpha(1).
                 rotation(3600).setDuration(1000);
+
+        Toast.makeText(this, tappedImageView.getTag().toString(), Toast.LENGTH_SHORT).show();
 
     }
 }
