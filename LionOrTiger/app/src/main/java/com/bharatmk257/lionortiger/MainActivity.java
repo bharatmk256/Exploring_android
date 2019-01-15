c8233b695c6e32b084309a3b0d47160c7d472519
bharatmk257 <bharatmk257@gmail.com>
2019-01-15 17:05:40 +0530

    some animations and grid layout and also images

--- LionOrTiger/app/src/main/java/com/bharatmk257/lionortiger/MainActivity.java
+++ LionOrTiger/app/src/main/java/com/bharatmk257/lionortiger/MainActivity.java
@@ -1,13 +1,46 @@
 package com.bharatmk257.lionortiger;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    enum Player {
+
+        ONE, TWO
+
+    }
+
+    Player currentPlayer = Player.ONE;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void imageViewIsTapped(View imageView) {
+
+        ImageView tappedImageView = (ImageView) imageView;  // this is casting
+
+        tappedImageView.setTranslationX(-2000);
+
+
+        if (currentPlayer == Player.ONE) {
+            tappedImageView.setImageResource(R.drawable.lion);
+            currentPlayer = Player.TWO;
+        }else if (currentPlayer == Player.TWO){
+            tappedImageView.setImageResource(R.drawable.tiger);
+            currentPlayer = Player.ONE;
+        }
+
+
+        tappedImageView.animate().translationXBy(2000).alpha(1).
+                rotation(3600).setDuration(1000);
+
     }
 }
