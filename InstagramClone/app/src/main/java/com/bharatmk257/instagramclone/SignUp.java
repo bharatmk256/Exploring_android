456f958e431e233ebbab4551ab5b4daa2b6c2cef
bharatmk257 <bharatmk257@gmail.com>
2019-01-18 21:11:38 +0530

    added some codes for getting data from server woa

--- InstagramClone/app/src/main/java/com/bharatmk257/instagramclone/SignUp.java
+++ InstagramClone/app/src/main/java/com/bharatmk257/instagramclone/SignUp.java
@@ -5,17 +5,30 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.parse.FindCallback;
+import com.parse.GetCallback;
 import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.SaveCallback;
 import com.shashank.sony.fancytoastlib.FancyToast;
+
+import java.util.List;
 
 public class SignUp extends AppCompatActivity implements View.OnClickListener {
 
     private Button btnSave;
     private EditText edtName, edtPunchSpeed, edtPunchPower, edtKickSpeed, edtKickPower;
+    private TextView txtGetData;
+
+    private Button btnGetAllData;
+
+    private String allKickBoxers;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,13 +36,75 @@ public class SignUp extends AppCompatActivity implements View.OnClickListener {
         setContentView(R.layout.activity_sign_up);
 
         btnSave = (Button) findViewById(R.id.btnSave);
+        btnSave.setOnClickListener(SignUp.this);
+
         edtName = (EditText) findViewById(R.id.edtName);
         edtPunchSpeed = (EditText) findViewById(R.id.edtPunchSpeed);
         edtPunchPower = (EditText) findViewById(R.id.edtPunchPower);
         edtKickSpeed = (EditText) findViewById(R.id.edtKickSpeed);
         edtKickPower = (EditText) findViewById(R.id.edtKickPower);
 
-        btnSave.setOnClickListener(SignUp.this);
+        txtGetData = (TextView) findViewById(R.id.txtGetData);
+
+        btnGetAllData = (Button) findViewById(R.id.btnGetAllData);
+
+        txtGetData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+
+                ParseQuery<ParseObject> parseQuery = ParseQuery.getQuery("KickBoxer");
+                parseQuery.getInBackground("e9LUQBbSrS", new GetCallback<ParseObject>() {
+                    @Override
+                    public void done(ParseObject object, ParseException e) {
+
+                        if (object != null && e == null){
+
+                            txtGetData.setText(object.get("name") + "-" + "Punch Power : " + object.get("punch_power"));
+
+                        }
+
+                    }
+                });
+            }
+        });
+
+
+        btnGetAllData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                allKickBoxers = "";
+
+                ParseQuery<ParseObject> queryAll = ParseQuery.getQuery("KickBoxer");
+                queryAll.findInBackground(new FindCallback<ParseObject>() {
+                    @Override
+                    public void done(List<ParseObject> objects, ParseException e) {
+
+                        if (e == null){
+
+                            if (objects.size() > 0){
+
+                                for (ParseObject kickBoxer : objects) {
+
+                                    allKickBoxers = allKickBoxers + kickBoxer.get("name") + "\n";
+
+
+                                }
+                                FancyToast.makeText(SignUp.this, allKickBoxers, FancyToast.LENGTH_SHORT, FancyToast.SUCCESS,true).show();
+                            } else {
+                                FancyToast.makeText(SignUp.this, e.getMessage(), FancyToast.LENGTH_SHORT, FancyToast.SUCCESS,true).show();
+
+                            }
+                        }
+
+                    }
+                });
+
+            }
+        });
+
 
     }
 
@@ -37,9 +112,6 @@ public class SignUp extends AppCompatActivity implements View.OnClickListener {
     public void onClick(View v) {
 
         try {
-
-
-
         ParseObject kickBoxer = new ParseObject("KickBoxer");
         kickBoxer.put("name",edtName.getText().toString());
         kickBoxer.put("punch_speed",Integer.parseInt(edtPunchSpeed.getText().toString()));
