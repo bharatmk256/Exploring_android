82ad2cc2eec334efc1beb968003f3ae5cd1a3aee
bharatmk257 <bharatmk257@gmail.com>
2019-02-20 16:25:09 +0530

    user can upload image

--- SocialMediaFirebase/app/src/main/java/com/bharatmk257/socialmediafirebase/SocialMediaActivity.java
+++ SocialMediaFirebase/app/src/main/java/com/bharatmk257/socialmediafirebase/SocialMediaActivity.java
@@ -4,9 +4,12 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Build;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -17,8 +20,17 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
+import java.util.UUID;
+import java.util.jar.Attributes;
 
 public class SocialMediaActivity extends AppCompatActivity {
 
@@ -28,6 +40,8 @@ public class SocialMediaActivity extends AppCompatActivity {
     private EditText edtDes;
     private ListView usersListView;
     private Bitmap bitmap;
+
+    private String imageIdentifier;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,46 +59,37 @@ public class SocialMediaActivity extends AppCompatActivity {
         postImageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-
+                selectImage();
             }
         });
 
         btnCreatePost.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-
+                if (bitmap == null) {
+                    Toast.makeText(SocialMediaActivity.this, "Please select image", Toast.LENGTH_SHORT).show();
+                } else {
+                    uploadImageToServer();
+                }
             }
         });
-
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-
         getMenuInflater().inflate(R.menu.my_menu, menu);
-
         return super.onCreateOptionsMenu(menu);
-
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-
         switch (item.getItemId()) {
-
             case R.id.logoutItem:
-
                 logout();
                 break;
-
         }
-
         return super.onOptionsItemSelected(item);
-
     }
-
 
     @Override
     public void onBackPressed() {
@@ -93,40 +98,79 @@ public class SocialMediaActivity extends AppCompatActivity {
     }
 
     private void logout() {
-
         mAuth.signOut();
         finish();
     }
 
-
     private void selectImage() {
-
         if (Build.VERSION.SDK_INT < 23) {
             Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
             startActivityForResult(intent, 1000);
-        } else if (Build.VERSION.SDK_INT >= 23) {
-
+        } else if (Build.VERSION.SDK_INT >= 23)
             if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-
                 requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1000);
-
             } else {
-
                 Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
                 startActivityForResult(intent, 1000);
-
             }
-
-        }
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-
-        if (requestCode == 1000 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+        if (requestCode == 1000 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
             selectImage();
         }
-
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1000 && resultCode == RESULT_OK && data != null) {
+            Uri chosenImageData = data.getData();
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), chosenImageData);
+                postImageView.setImageBitmap(bitmap);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void uploadImageToServer() {
+
+        if (bitmap != null) {
+
+            // Get the data from an ImageView as bytes
+            postImageView.setDrawingCacheEnabled(true);
+            postImageView.buildDrawingCache();
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, baos);
+            byte[] data = baos.toByteArray();
+
+            imageIdentifier = UUID.randomUUID() + ".png";
+
+            UploadTask uploadTask = FirebaseStorage.getInstance().getReference().child("my_images").child(imageIdentifier).putBytes(data);
+            uploadTask.addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception exception) {
+                    // Handle unsuccessful uploads
+
+                    Toast.makeText(SocialMediaActivity.this, exception.toString(), Toast.LENGTH_SHORT).show();
+
+                }
+            }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    // taskSnapshot.getMetadata() contains file metadata such as size, content-type, etc.
+                    // ...
+
+                    Toast.makeText(SocialMediaActivity.this, "Success", Toast.LENGTH_SHORT).show();
+
+                    edtDes.setVisibility(View.VISIBLE);
+                }
+            });
+        }
+    }
+
 }
